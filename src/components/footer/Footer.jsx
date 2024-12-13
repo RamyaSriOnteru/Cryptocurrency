b357965732a62dae21982d7ef79c246e2e39629c
RamyaSriOnteru <ramyasri@synergytechs.net>
2024-12-13 18:03:46 +0530

    add changes

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright &copy; 2024, Crytoplace - All Rights Reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
